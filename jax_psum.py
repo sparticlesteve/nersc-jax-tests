926c099d8e511aee028b26efdf56815d96259392
Steve Farrell <sparticlesteve@gmail.com>
2024-10-18 11:22:10 -0700

    printout updates

--- jax_psum.py
+++ jax_psum.py
@@ -5,12 +5,10 @@ import sys
 
 def run():
     rank = int(os.environ["SLURM_PROCID"])
-    print('Rank %d running pid %d'%(rank, os.getpid()))
     address = os.environ['MASTER_ADDR']+':29500'
-    print(address)
+    print(f'Rank {rank} coordinator {address}')
     jax.distributed.initialize(coordinator_address=address)
-    print(jax.device_count())  # total number of accelerator devices in the cluster
-    print(jax.local_device_count())  # number of accelerator devices attached to this host
+    print(f'devices {jax.device_count()}', f'local_devices {jax.local_device_count()}')
     # The psum is performed over all mapped devices across the pod slice
     xs = jax.numpy.ones(jax.local_device_count())
     print('Performing psum')
