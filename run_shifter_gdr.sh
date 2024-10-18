43adf9ea4e9ffe14a91a9abeda47e3ad48eaa429
Steve Farrell <sparticlesteve@gmail.com>
2024-10-18 11:22:40 -0700

    adding a version with manual setup of libgdrapi

---
+++ run_shifter_gdr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -J jax-psum
+#SBATCH -C gpu
+#SBATCH -N 2
+#SBATCH --ntasks-per-node=4
+#SBATCH --gpus-per-node=4
+#SBATCH --image=ghcr.io/nvidia/jax:jax-2024-09-08
+#SBATCH --module=gpu,nccl-plugin
+
+export MASTER_ADDR=$(hostname)
+#export FI_LOG_LEVEL=trace
+export NCCL_DEBUG=INFO
+
+# Manual setup of GDR, can be done before running job
+mkdir -p $SCRATCH/shifter-gdr
+rsync -vau /usr/lib64/libgdrapi.so* $SCRATCH/shifter-gdr/
+
+srun -u -l shifter bash -c "
+    export LD_LIBRARY_PATH=$SCRATCH/shifter-gdr:$LD_LIBRARY_PATH
+    python jax_psum.py
+"
